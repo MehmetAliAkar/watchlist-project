f931765fd1e8b0d2704574c11ac179241d4a7d8f
Mehmet Ali AKAR <maliakar123@gmail.com>
2024-03-05 00:51:38 +0300

    init project

---
+++ src/main/java/com/sisterslab/watchlistproject/WatchlistProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sisterslab.watchlistproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchlistProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchlistProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sisterslab/watchlistproject/WatchlistProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisterslab.watchlistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
